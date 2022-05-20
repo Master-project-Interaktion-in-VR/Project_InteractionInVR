5ec7a0513d6836a8f1e846a6891f5d2452ec9e6c
Samuel Günther <samuell.guenther@gmail.com>
2022-05-20 17:39:20 +0200

    multiplayer draw working concept WIP

--- Assets/Scripts/XDPaintTest.cs
+++ Assets/Scripts/XDPaintTest.cs
@@ -4,28 +4,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using XDPaint;
 using XDPaint.Controllers;
+using XDPaint.Core;
+using XDPaint.Core.PaintObject.Base;
 
 public class XDPaintTest : MonoBehaviour
 {
 
     public PaintManager _pm;
-    private Renderer _screen;
-    private Renderer _plane;
+    public PaintManager _screen;
+    //private Renderer _plane;
 
-    private Texture2D _screenTex;
+    //private Texture2D _screenTex;
+    private Vector2 _previousPoint;
 
     void Start()
     {
-        _screen = GameObject.Find("Screen").GetComponent<Renderer>();
-        _plane = GameObject.Find("Plane").GetComponent<Renderer>();
-        _screenTex = (Texture2D)_screen.material.mainTexture;
-        _screen.material.mainTexture = _screenTex;
+        //_screen = GameObject.Find("Screen").GetComponent<Renderer>();
+        //_plane = GameObject.Find("Plane").GetComponent<Renderer>();
+        //_screenTex = (Texture2D)_screen.material.mainTexture;
+        //_screen.material.mainTexture = _screenTex;
         _pm.OnInitialized += OnPaintManagerInitialized;
+        //_pm.ToolsManager.CurrentTool.OnPaint;
+
     }
 
     private void OnPaintManagerInitialized(PaintManager paintManager)
     {
         _pm = paintManager;
+        _pm.PaintObject.OnPaintDataHandler += OnPaint;
+        _pm.PaintObject.OnDrawLineHandler += Abc;
+    }
+
+    private void Abc(BasePaintObject sender, Vector2 lineStartPosition, Vector2 lineEndPosition, float lineStartPressure, float lineEndPressure)
+    {
+        _screen.PaintObject.DrawLine(lineStartPosition, lineEndPosition);
+    }
+
+    private void OnPaint(BasePaintObject sender, Vector2 paintPosition, float brushSize, float pressure, Color brushColor, PaintTool tool)
+    {
+        ////_screen.ToolsManager.CurrentTool.OnPaint(sender, paintPosition, pressure);
+        //if (_previousPoint != null)
+        //    _screen.PaintObject.DrawLine(_previousPoint, paintPosition);
+        //_previousPoint = paintPosition;
     }
 
     private void FixedUpdate()
@@ -36,23 +56,23 @@ public class XDPaintTest : MonoBehaviour
         }
     }
 
-    public byte[] GetRenderTexturePixels(RenderTexture tex)
-    {
-        RenderTexture.active = tex;
-        Texture2D tempTex = new Texture2D(tex.width, tex.height);
-        tempTex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
-        tempTex.Apply();
-        //return tempTex.GetPixels();
-        byte[] bytes = tempTex.GetRawTextureData();
-        Destroy(tempTex);
-        return bytes;
-    }
+    //public byte[] GetRenderTexturePixels(RenderTexture tex)
+    //{
+    //    RenderTexture.active = tex;
+    //    Texture2D tempTex = new Texture2D(tex.width, tex.height);
+    //    tempTex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+    //    tempTex.Apply();
+    //    //return tempTex.GetPixels();
+    //    byte[] bytes = tempTex.GetRawTextureData();
+    //    Destroy(tempTex);
+    //    return bytes;
+    //}
 
     private void SendBytes()
     {
         //Texture2D resTex = _pm.GetResultTexture();
-        RenderTexture resTex = _pm.GetPaintTexture();
-        byte[] bytes = GetRenderTexturePixels(resTex);
+        //RenderTexture resTex = _pm.GetPaintTexture();
+        //byte[] bytes = GetRenderTexturePixels(resTex);
 
 
 
