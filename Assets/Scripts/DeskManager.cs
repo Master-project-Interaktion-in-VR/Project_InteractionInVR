7f076e33eb4c1fb1e74c892e4f8fcb6474028c48
Faye Bullwinkel <faye.bullwinkel@web.de>
2022-06-22 07:39:04 +0200

    added desk appearing/disappearing logic if player is in range

---
+++ Assets/Scripts/DeskManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskManager : MonoBehaviour
+{
+    private GameObject rockTable;
+    public void Start()
+    {
+        rockTable = GameObject.Find("RockTable");
+        rockTable.GetComponentInChildren<Renderer>().enabled = false;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entering!");
+        if (other.gameObject.name == "PlayerEmpty")
+        {
+            rockTable.GetComponentInChildren<Renderer>().enabled = true;
+        }
+    }
+}
