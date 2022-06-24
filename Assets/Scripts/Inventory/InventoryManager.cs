6af773e0e4a58a50e555200c0f07358eb6164507
Emircan Y <emir-00@live.de>
2022-06-24 23:05:17 +0200

    removed unused scripts

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -21,16 +21,6 @@ public class InventoryManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space) && itemInHand != null)
-        {
-            items.Add(itemInHand);
-            itemInHand.SetActive(false);
-            itemInHand = null;
-        }
-        else if (Input.GetKey(KeyCode.G) && itemInHand != null)
-        {
-            itemInHand.GetComponent<PickUp>().SolverHandler.UpdateSolvers = false;
-            itemInHand = null;
-        }
+        
     }
 }
