44a2e2d4825a03829bab4306ce26d0e964340534
Emircan Y <emir-00@live.de>
2022-06-26 07:52:03 +0200

    teleportation range | direct interactables | inventory

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -2,25 +2,33 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class InventoryManager : MonoBehaviour
 {
-    [SerializeField] private GameObject itemInHand;
+    //[SerializeField] private GameObject itemInHand;
 
-    [SerializeField] private List<GameObject> items = new List<GameObject>();
+    [SerializeField] private Inventory inventory;
 
-    public void PutItemInHand(GameObject item)
+    private void Start()
     {
-        itemInHand = item;
+        inventory.antennaParts = new List<GameObject>();
     }
 
-    public bool HandIsFree()
-    {
-        return itemInHand == null;
-    }
+    // public void PutItemInHand(GameObject item)
+    // {
+    //     itemInHand = item;
+    // }
+    //
+    // public bool HandIsFree()
+    // {
+    //     return itemInHand == null;
+    // }
 
-    private void Update()
+    public void PutItemInInventory(ActivateEventArgs args)
     {
-        
+        var item = args.interactableObject.transform.gameObject;
+        inventory.antennaParts.Add(item);
+        item.SetActive(false);
     }
 }
