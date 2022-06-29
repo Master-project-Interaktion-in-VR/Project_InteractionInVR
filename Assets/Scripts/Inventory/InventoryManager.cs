b8dd15ba0aa88d2310d7c3ada58f7f49949f3cc2
Fatlind Krasniqi <s0564648@htw-berlin.de>
2022-06-29 08:13:55 +0200

    ui setup and interactable | take item from inventory variant 1

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -9,6 +10,20 @@ public class InventoryManager : MonoBehaviour
 {
     [SerializeField] private int antennaPartsPickedUp;
     [SerializeField] private int maxAntennaParts;
+
+    public GameObject rightHandModel;
+
+    public Transform handSpawn;
+
+    public List<GameObject> inventoryItems = new List<GameObject>();
+
+    public Button stickButton;
+    public Button detectorButton;
+
+    void Start()
+    {
+       // PutItemInHand();    
+    }
 
     public void PutItemInInventory(ActivateEventArgs args)
     {
@@ -22,8 +37,53 @@ public class InventoryManager : MonoBehaviour
         }
     }
 
+    public void DeinVater()
+    {
+        rightHandModel.GetComponent<MeshRenderer>().enabled = false;
+    }
+
     private void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PutStickInHand()
+     {
+         for (int i = 0; i < inventoryItems.Count; i++)
+         {
+             if (inventoryItems[i].name == "Stick")
+             {
+                 Instantiate(inventoryItems[i], handSpawn.position, handSpawn.rotation);
+                 inventoryItems.Remove(inventoryItems[i]);
+                 rightHandModel.GetComponent<MeshRenderer>().enabled = false;
+                 stickButton.interactable = false;
+                 detectorButton.interactable = true;
+                //stickButton.gameObject.SetActive(false);
+            }
+             /*else if (inventoryItems[i].name == "MetalDetector")
+             {
+                 Instantiate(inventoryItems[i], handSpawn.position, handSpawn.rotation);
+                 detectorButton.interactable = false;
+                 stickButton.interactable = true;
+            }*/
+         }
+     }
+
+    public void PutDetectorInHand()
+    {
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].name == "MetalDetector")
+            {
+                Instantiate(inventoryItems[i], handSpawn.position, handSpawn.rotation);
+                inventoryItems.Remove(inventoryItems[i]);
+                rightHandModel.GetComponent<MeshRenderer>().enabled = false;
+                stickButton.interactable = false;
+                detectorButton.interactable = true;
+                //stickButton.gameObject.SetActive(false);
+                GameObject stick = GameObject.Find("Stick");
+                inventoryItems.Add(stick);
+            }
+        }
     }
 }
