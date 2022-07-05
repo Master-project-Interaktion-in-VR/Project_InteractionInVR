9211088812a362a6c3b6991c0a2012ccb6a84fcb
Emircan Y <emir-00@live.de>
2022-07-05 22:04:17 +0200

    Updated Environment Scene with Pickup, Inventory and XRI Motion | Added Item models and teleportation delay

--- Assets/Scripts/TeleportationController.cs
+++ Assets/Scripts/TeleportationController.cs
@@ -4,17 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class TeleportationController : MonoBehaviour
 {
-    [SerializeField] private GameObject controller;
-    [SerializeField] private GameObject teleportationRay;
+    [SerializeField] private ActionBasedController controller;
+    [SerializeField] private XRRayInteractor rayInteractor;
+    [SerializeField] private InteractionLayerMask teleportationLayerMask;
+    [SerializeField] private InteractionLayerMask noTeleportationLayerMask;
 
     [SerializeField] private InputActionReference teleportActivationReference;
 
-    [Space]
-    [SerializeField] private UnityEvent onTeleportActivate;
-    [SerializeField] private UnityEvent onTeleportCancel;
+    [SerializeField] private float startVelocity;
+    [SerializeField] private float maxVelocity;
+    [SerializeField] private float increaseStep;
+    [SerializeField] private float increaseDelay;
+
+    private bool _deactivated;
 
     private void Start()
     {
@@ -22,9 +29,39 @@ public class TeleportationController : MonoBehaviour
         teleportActivationReference.action.canceled += CancelTeleport;
     }
 
-    private void ActivateTeleport(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
+    private void ActivateTeleport(InputAction.CallbackContext obj)
+    {
+        rayInteractor.enabled = true;
+        controller.enableInputActions = true;
+        _deactivated = false;
+        rayInteractor.interactionLayers = noTeleportationLayerMask;
+        StartCoroutine(IncreaseVelocity());
+    }
 
-    private void CancelTeleport(InputAction.CallbackContext obj) => Invoke(nameof(DeactivateTeleport), .1f);
+    private void CancelTeleport(InputAction.CallbackContext obj)
+    {
+        _deactivated = true;
+        Invoke(nameof(DeactivateTeleport), .1f);
+    }
 
-    private void DeactivateTeleport() => onTeleportCancel.Invoke();
+    private void DeactivateTeleport()
+    {
+        rayInteractor.enabled = false;
+        controller.enableInputActions = false;
+        rayInteractor.velocity = startVelocity;
+    }
+
+    private IEnumerator IncreaseVelocity()
+    {
+        while (rayInteractor.velocity < maxVelocity && !_deactivated)
+        {
+            yield return new WaitForSeconds(increaseDelay);
+            rayInteractor.velocity += increaseStep;
+        }
+
+        if (rayInteractor.velocity >= maxVelocity)
+        {
+            rayInteractor.interactionLayers = teleportationLayerMask;
+        }
+    }
 }
