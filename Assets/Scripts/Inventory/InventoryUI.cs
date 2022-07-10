40512de46247ce197f4251e5ca2cdd7f26c6585a
Fatlind Krasniqi <s0564648@htw-berlin.de>
2022-07-10 14:43:23 +0200

    inventory in both hands | images in inventorys with arrows | cleaned up code | input actions for handling inventory | setup for further code

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -5,21 +5,26 @@ using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
-    public GameObject Inventory;
-    public GameObject Anchor;
-    bool UIActive;
+    public Image image;
+    public Sprite newSprite;
 
-    private void Update()
+
+    /* private void Update()
+     {
+         if (Input.GetKeyDown(KeyCode.DownArrow))
+         {
+             UIActive = !UIActive;
+             Inventory.SetActive(UIActive);
+         }
+         if (UIActive)
+         {
+             Inventory.transform.position = Anchor.transform.position;
+             Inventory.transform.eulerAngles = new Vector3(Anchor.transform.eulerAngles.x + 15, Anchor.transform.eulerAngles.y, 0);
+         }
+     }*/
+
+    public void ImageChange()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            UIActive = !UIActive;
-            Inventory.SetActive(UIActive);
-        }
-        if (UIActive)
-        {
-            Inventory.transform.position = Anchor.transform.position;
-            Inventory.transform.eulerAngles = new Vector3(Anchor.transform.eulerAngles.x + 15, Anchor.transform.eulerAngles.y, 0);
-        }
+        image.sprite = newSprite;
     }
 }
