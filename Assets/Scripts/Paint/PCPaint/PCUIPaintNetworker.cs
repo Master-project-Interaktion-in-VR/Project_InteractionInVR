48a3df70eb601fa72b3495fde90a6fe4be128d12
Samuel Günther <samuell.guenther@gmail.com>
2022-07-16 19:41:18 +0200

    fixed PC drawing, added laptop

--- Assets/Scripts/Paint/PCPaint/PCUIPaintNetworker.cs
+++ Assets/Scripts/Paint/PCPaint/PCUIPaintNetworker.cs
@@ -12,66 +12,71 @@ using XDPaint.Tools.Raycast;
 public class PCUIPaintNetworker : MonoBehaviour
 {
 
-		[SerializeField]
-		private float delay;
+    [SerializeField]
+    private Vector2 screenTextureSize;
 
 
-		private PaintManager _paintManager;
-		private PhotonView _photonView;
+    private PaintManager _paintManager;
+    private PhotonView _photonView;
 
-		private Vector2 _previousPoint;
-		private float _startTime;
+    private Vector2 _previousPoint;
+    private float _startTime;
 
-		private Vector2 lastPoint;
-
-
-		void Awake()
-		{
-				_photonView = GetComponent<PhotonView>();
-				_paintManager = GetComponent<PaintManager>();
-				// _paintManager.OnInitialized += OnPaintManagerInitialized;
-		}
-
-		// private void OnPaintManagerInitialized(PaintManager paintManager)
-		// {
-		//     //_paintManager = paintManager;
-		//     //_paintManager.PaintObject.OnPaintDataHandler += OnPaint;
-		//     // _paintManager.PaintObject.OnMouseHandler += OnMouseDown;
-		//     // _paintManager.PaintObject.OnMouseUpHandler += OnMouseUp;
-		// }
+    private Vector2 lastPoint;
 
 
+    void Awake()
+    {
+        _photonView = GetComponent<PhotonView>();
+        _paintManager = GetComponent<PaintManager>();
+        // _paintManager.OnInitialized += OnPaintManagerInitialized;
+    }
 
-		[PunRPC]
-		private void DrawLineRpc(Vector2 lineStartPosition, Vector2 lineEndPosition)
-		{
-				_paintManager.PaintObject.DrawLine(lineStartPosition, lineEndPosition);
-		}
+    // private void OnPaintManagerInitialized(PaintManager paintManager)
+    // {
+    //     //_paintManager = paintManager;
+    //     //_paintManager.PaintObject.OnPaintDataHandler += OnPaint;
+    //     // _paintManager.PaintObject.OnMouseHandler += OnMouseDown;
+    //     // _paintManager.PaintObject.OnMouseUpHandler += OnMouseUp;
+    // }
 
-		[PunRPC]
-		private void DrawPointRpc(Vector2 position)
-		{
-				Vector2 scaledPosition = new Vector2(position.x * 2048, position.y * 1024);
-				Debug.Log("DrawPointRpc" + scaledPosition);
 
-				if (lastPoint == null)
-				{
-						lastPoint = scaledPosition;
-				}
 
-				_paintManager.PaintObject.DrawLine(scaledPosition, scaledPosition);
-				// _paintManager.PaintObject.DrawPoint(scaledPosition, 30);
+    [PunRPC]
+    private void DrawLineRpc(Vector2 lineStartPosition, Vector2 lineEndPosition)
+    {
+        _paintManager.PaintObject.DrawLine(lineStartPosition, lineEndPosition);
+    }
 
-				lastPoint = scaledPosition;
-		}
+    [PunRPC]
+    private void DrawPointRpc(Vector2 position)
+    {
+        Vector2 scaledPosition = new Vector2(position.x * screenTextureSize.x, position.y * screenTextureSize.y);
+        Debug.Log("DrawPointRpc" + scaledPosition);
 
-		[PunRPC]
-		private void ResetDrawingRpc()
-		{
-				Debug.Log("ResetDrawingRpc");
-				_paintManager.PaintObject.DoDispose();
-				_paintManager.PaintObject.ClearTexture();
-				_paintManager.PaintObject.Render();
+        if (Vector2.Distance(lastPoint, Vector2.zero) == 0)
+        {
+            lastPoint = scaledPosition;
+        }
 
-		}
+        _paintManager.PaintObject.DrawLine(lastPoint, scaledPosition);
+        // _paintManager.PaintObject.DrawPoint(scaledPosition, 30);
+
+        lastPoint = scaledPosition;
+    }
+
+    [PunRPC]
+    private void MouseUpRpc()
+    {
+        lastPoint = Vector2.zero;
+    }
+
+    [PunRPC]
+    private void ResetDrawingRpc()
+    {
+        Debug.Log("ResetDrawingRpc");
+        //_paintManager.PaintObject.DoDispose();
+        _paintManager.PaintObject.ClearTexture();
+        _paintManager.Render();
+    }
 }
