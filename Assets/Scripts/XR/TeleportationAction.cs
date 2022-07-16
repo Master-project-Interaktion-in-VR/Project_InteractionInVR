ccbfec1d2f07a58a9089bd787d5da965263db772
Emircan Y <emir-00@live.de>
2022-07-17 01:57:31 +0200

    fade screen for scene transitions and teleportation

--- Assets/Scripts/XR/TeleportationAction.cs
+++ Assets/Scripts/XR/TeleportationAction.cs
@@ -6,14 +6,22 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class TeleportationAction : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
-    [SerializeField] private Animator fadeAnimator;
+    [SerializeField] private FadeScreen fadeScreen;
+    [SerializeField] private float fadeDuration;
     [SerializeField] private float hapticIntensity;
-    [SerializeField] private float duration;
+    [SerializeField] private float hapticDuration;
 
     public void Teleport(TeleportingEventArgs args)
     {
+        StartCoroutine(FadeCoroutine());
         audioSource.Play();
-        args.interactorObject.transform.GetComponent<XRBaseController>().SendHapticImpulse(hapticIntensity, duration);
-        fadeAnimator.SetTrigger("FadeOut");
+        args.interactorObject.transform.GetComponent<XRBaseController>().SendHapticImpulse(hapticIntensity, hapticDuration);
+    }
+
+    private IEnumerator FadeCoroutine()
+    {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(fadeDuration);
+        fadeScreen.FadeIn(fadeDuration);
     }
 }
