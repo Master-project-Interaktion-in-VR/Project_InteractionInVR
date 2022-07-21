9c31b3cb4eab1b51e9a70ef88f742f5606d3b377
Samuel Günther <samuell.guenther@gmail.com>
2022-07-22 01:07:16 +0200

    small changes

--- Assets/Scripts/Avatar/VRHands/TakeoverOwnership.cs
+++ Assets/Scripts/Avatar/VRHands/TakeoverOwnership.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 
 public class TakeoverOwnership : MonoBehaviour
 {
+    private PhotonView _photonView;
+
+    private void Awake()
+    {
+        _photonView = GetComponent<PhotonView>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         // request ownership of collider object if VR player touches them to be able to move them over the network
-        if (other.gameObject.layer == LayerMask.NameToLayer("VrHands"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("VrHands") && !_photonView.IsMine)
+        {
             GetComponent<PhotonView>().RequestOwnership();
+            //Debug.LogError("TAKE OWNERSHIP: " + _photonView.IsMine);
+        }
     }
 }
